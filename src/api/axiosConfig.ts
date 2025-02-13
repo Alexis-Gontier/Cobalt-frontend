a16758ef03e7034011693b811c97209296b6e6d8
Gontier <alexis.gontier03@gmail.com>
2025-02-13 20:02:41 +0100

    Mettre à jour l'URL de base d'Axios pour pointer vers le serveur de production

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosConfig = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "http://15.237.52.83",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
