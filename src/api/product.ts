f131469222f1c4e9e49b42d539b7c086e9d59332
Alexis G <alexis.gontier03@gmail.com>
2025-02-13 09:56:59 +0100

    Ajout de la fonction de récupération des produits et correction de la fin de fichier dans le module d'enregistrement

---
+++ src/api/product.ts
@@ -0,0 +1,15 @@
+import axiosConfig from "./axiosConfig";
+
+export const product = async (
+  id: string,
+  titre: string,
+  description: string,
+  prix: number,
+  quantite: number,
+  image_url: string
+) => {
+  const response = await axiosConfig.get("/", {
+    params: { id, titre, description, prix, quantite, image_url },
+  });
+  return response.data;
+};
