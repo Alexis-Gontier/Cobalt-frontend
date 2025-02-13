0969836459d672043f5bb382a0d933ccd73685f5
Alexis G <alexis.gontier03@gmail.com>
2025-02-13 10:11:01 +0100

    Ajout de la fonction d'exportation des produits dans l'API

--- src/api/product.ts
+++ src/api/product.ts
@@ -12,4 +12,18 @@ export const product = async (
     params: { id, titre, description, prix, quantite, image_url },
   });
   return response.data;
-};
+}
+
+export const exportProduct = async (
+  id: string,
+  titre: string,
+  description: string,
+  prix: number,
+  quantite: number,
+  image_url: string
+) => {
+  const response = await axiosConfig.post("/", {
+    params: { id, titre, description, prix, quantite, image_url },
+  })
+  return response.data;
+}
