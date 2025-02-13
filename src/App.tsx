de5634bf0803dae94e3aa049bc531b08e4ddca2a
Gontier <alexis.gontier03@gmail.com>
2025-02-13 23:36:38 +0100

    Modifier les routes pour rediriger vers le composant ProductById et ajouter des redirections pour une meilleure navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { RegisterForm } from "@/components/auth/registerForm/registerForm";
 import { LoginForm } from "@/components/auth/loginForm/loginForm";
 import { AuthLayout } from "@/layouts/authLayout";
 import MainLayout from "@/layouts/MainLayout";
 import ProductScroll from "@/components/products/ProductsScroll";
+import ProductById from "@/components/products/ProductById";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<MainLayout />}>
-          <Route path="/" element={<ProductScroll />} />
-          <Route path="/products/:id" element={<h1>Hey !</h1>} />
+          <Route index element={<ProductScroll />} />
+          <Route path="/product/" element={<Navigate to={"/"} />} />
+          <Route path="/product/:id" element={<ProductById />} />
         </Route>
-        <Route path="/products" element={<ProductScroll />} />
+
         <Route path="/auth" element={<AuthLayout />}>
+          <Route index element={<Navigate to="/auth/login" />} />
           <Route path="register" element={<RegisterForm />} />
           <Route path="login" element={<LoginForm />} />
         </Route>
+
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
   );
