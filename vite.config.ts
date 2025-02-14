a5f0691e115b75f2ac57bf4c8b41c40b344e417a
Alexis G <alexis.gontier03@gmail.com>
2025-02-14 10:05:24 +0100

    Ajouter le support PWA avec vite-plugin-pwa et mettre à jour les icônes dans index.html

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,37 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,ico,png}'],
+      },
+      manifest: {
+        name: 'React Vite Tailwind',
+        short_name: 'React Vite Tailwind',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: './',
 });
